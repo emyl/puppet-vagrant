48285d3b314619aa16d5aabf16f440232f5cccb7
Lukas Stanek <ls@elostech.cz>
2014-11-26 18:52:27 +0100

    Support for managing plugin versions.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -11,10 +11,23 @@
 #   The user under which the plugin will be added. Ignored on Windows
 #   systems (current user is assumed).
 #
-define vagrant::plugin($plugin_name = $title, $user = $::id) {
+define vagrant::plugin(
+  $plugin_name    = $title,
+  $user           = $::id
+  $plugin_version = undef,
+) {
   include vagrant::params
 
+  if $plugin_version == undef {
+    $plugin_command = "${vagrant::params::binary} plugin install ${plugin_name}"
+    $plugin_unless = "${vagrant::params::binary} plugin list | ${vagrant::params::grep} \"^${plugin_name}\s\""
+  } else {
+    $plugin_command = "${vagrant::params::binary} plugin install ${plugin_name} --plugin-version ${plugin_version}"
+    $plugin_unless = "${vagrant::params::binary} plugin list | ${vagrant::params::grep} \"^${plugin_name}\s(${plugin_version})\""
+  }
+
   vagrant::command { "${vagrant::params::binary} plugin install ${plugin_name}":
-    user => $user
+    unless => $plugin_unless,
+    user   => $user,
   }
 }
